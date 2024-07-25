db82756f563373b26b6715349dce4c9c2a6a9696
Aryan kholqi <aryankholqi@gmail.com>
2024-07-25 20:13:34 +0330

    filter data in client, make responsive

--- src/components/Templates/Home/AssetCard.tsx
+++ src/components/Templates/Home/AssetCard.tsx
@@ -3,16 +3,36 @@ import ArrowUpRightIcon from "../../../assets/icons/Arrow-Up-Right";
 import { assetCardProps } from "../../../interfaces/assetCard.interface";
 import MainBarChart from "../../Modules/Charts/MainBarChart";
 import convertToValidBarChartData from "../../../utility/convertToValidBarChartData";
+import { useLocation, useNavigate } from "@tanstack/react-router";
 
 export default function AssetCard({ title, ips, live, monitored, ports, total, total_live, total_monitored, mainIconInfo, bottomStatsIconInfo, vulns }: assetCardProps) {
+    const navigate = useNavigate()
+    const { pathname, search } = useLocation()
+    /* @ts-expect-error type is not a part of search structure as default */
+    const { type } = search
+
     const live_monitored_data = [
         { tilte: "Live", total: total_live, chartData: live },
         { tilte: "Monitored", total: total_monitored, chartData: monitored },
     ]
 
     const bottomStatsDigits = [ips, ports, vulns]
+
+    const selectAssetTypeToFilterData = (title: string) => {
+        if (title.toLowerCase().includes(type?.toLowerCase())) {
+            navigate({
+                to: pathname
+            })
+        } else {
+            navigate({
+                to: pathname,
+                search: ((prev) => ({ ...prev, type: title }))
+            })
+        }
+    }
     return (
-        <div className="bg-[#1D2229] p-3 rounded-xl space-y-4">
+        < div className={`bg-[#1D2229] p-3 rounded-xl space-y-4 transition-all duration-[120ms] cursor-pointer ${type?.includes(title) ? "border-2 border-white" : null}`}
+            onClick={() => selectAssetTypeToFilterData(title)}>
             <div className="flex justify-between items-start text-center">
                 <div className="flex flex-col">
                     <span className={`py-2 px-4 rounded-tl-lg rounded-tr-lg`} style={{ backgroundColor: mainIconInfo.bgColor }}>
@@ -28,7 +48,7 @@ export default function AssetCard({ title, ips, live, monitored, ports, total, t
                 <h1>{title}</h1>
                 <Divider className="bg-white h-[.5px]" />
             </div>
-            <div className="grid grid-cols-2 gap-2">
+            <div className="grid grid-cols-1 2xs:grid-cols-2 sm:grid-cols-1 lg:grid-cols-2 gap-4">
                 {live_monitored_data.map((item, index) => (
                     <div key={index} className="flex items-end gap-4">
                         <div className="flex flex-col">
@@ -38,11 +58,11 @@ export default function AssetCard({ title, ips, live, monitored, ports, total, t
                         <MainBarChart data={convertToValidBarChartData(item.chartData)} />
                     </div>
                 ))}
-                <Divider className="col-span-2 bg-white h-[.5px] mt-4" />
+                <Divider className="col-span-1 2xs:col-span-2 sm:col-span-1 lg:col-span-2 bg-white h-[.5px] mt-4" />
             </div>
-            <div className="grid grid-cols-3 gap-4">
+            <div className="flex items-center flex-wrap gap-4">
                 {bottomStatsIconInfo.map((item, index) => (
-                    <div key={index} className="flex gap-2 items-center">
+                    <div key={index} className="flex gap-2 items-center flex-grow">
                         <span className="bg-[#327794] p-2 rounded-md">
                             {item.Icon}
                         </span>
@@ -53,6 +73,6 @@ export default function AssetCard({ title, ips, live, monitored, ports, total, t
                     </div>
                 ))}
             </div>
-        </div>
+        </div >
     )
 }
--- src/hooks/api/useAssetApi.ts
+++ src/hooks/api/useAssetApi.ts
@@ -1,8 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAssetsInfoApi } from "../../services/api/assetApi";
+import { useLocation } from "@tanstack/react-router";
+import { assetProps } from "../../interfaces/asset.interface";
 
-export const useGetAssetsInfoApi = () =>
-  useQuery({
-    queryKey: ["assetsInfo"],
-    queryFn: () => getAssetsInfoApi().then((data) => data.data),
+export const useGetAssetsInfoApi = () => {
+  const { search } = useLocation();
+  //@ts-expect-error type is not a part of search structure as default
+  const { type } = search;
+
+  return useQuery({
+    queryKey: ["assetsInfo", type],
+    queryFn: () =>
+      getAssetsInfoApi().then((response) => {
+        const result = response.data;
+
+        if (type) {
+          const filteredAssets = result.assets.filter((item: assetProps) =>
+            type.toLowerCase().includes(item.type.toLowerCase())
+          );
+          return { ...result, assets: filteredAssets };
+        }
+
+        return result;
+      }),
   });
+};
